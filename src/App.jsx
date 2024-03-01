91d8003152161acae639d9b89b27b01739652f97
hackerharsh-77 <r.harsh0177@gmail.com>
2024-03-01 17:42:56 +0530

    redux store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {Header} from "./components/";
+import { Footer } from './components';
+import { login } from './store/authSlice';
+import { logout } from './store/authSlice';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog with apprwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+      authService.getCurrentUser()
+      .then((useData)=>{
+        if(useData){
+          dispatch(login(useData))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=>{
+        setLoading(false)
+      })
+  },[])
+
+  return !loading ? <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className="w-full block">
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  </div> : null
+
+  // return (
+  //   <>
+  //     <h1>A blog with apprwrite</h1>
+  //   </>
+  // )
 }
 
 export default App
